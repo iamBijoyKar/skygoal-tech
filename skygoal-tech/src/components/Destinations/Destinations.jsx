88cca4ae5114b46f744ba904652299b93017124a
iamBijoyKar <bijoypoco12345@gmail.com>
2023-03-16 22:34:27 +0530

    slider bug fix

--- skygoal-tech/src/components/Destinations/Destinations.jsx
+++ skygoal-tech/src/components/Destinations/Destinations.jsx
@@ -8,32 +8,25 @@ import { useState } from 'react';
 export default function Destinations(){
     const [slide,setSlide] = useState(1);
     const totalSlides = 5;
-    const slider = document.querySelector('#slides');
     const card = document.querySelector('.des-card');
-    
     const leftSlide = (e) =>{
         console.log('clicked');
-        if(card && slider){
-            slider.scrollLeft -= card.clientWidth;
-            if(slide == 1){
-                return ;
-            }
-            else{
-                setSlide(slide-1);
-            }
-
+        document.querySelector('#slides').scrollLeft -=document.querySelector('.des-card').clientWidth;
+        if(slide == 1){
+            return ;
+        }
+        else{
+            setSlide(slide-1);
         }
     }
     const rightSlide = () =>{
         console.log('clicked');
-        if(card && slider){
-            slider.scrollLeft += card.clientWidth;
-            if(slide == totalSlides){
-                return ;
-            }
-            else{
-                setSlide(slide+1);
-            }
+        document.querySelector('#slides').scrollLeft += document.querySelector('.des-card').clientWidth;
+        if(slide == totalSlides){
+            return ;
+        }
+        else{
+            setSlide(slide+1);
         }
     }
     return(
